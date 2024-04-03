351bb704b6b7da2ffb79a5e2c0fb0de4d3632f55
thakordixit567 <103193972+thakordixit567@users.noreply.github.com>
2024-04-03 17:02:16 +0530

    UPDATE!

--- dix-news/src/Componets/NewsItem.jsx
+++ dix-news/src/Componets/NewsItem.jsx
@@ -5,7 +5,7 @@ const NewsItem = ({ title, description, src, url }) => {
   return (
     <div
       className="card bg-dark text-light mb-3 d-inline-block my-3 mx-5 px-2 py-2"
-      style={{ maxWidth: "345px" }}
+      style={{ maxWidth: "350px" }}
     >
       <img
         src={src?src:image}
